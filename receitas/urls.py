9df6fb4521d853eb04af50c0dd84f1580b7e792f
Alexandre Fagundes <alefagundes155@gmail.com>
2025-01-11 08:35:58 -0300

    djangoRemove

--- receitas/urls.py
+++ receitas/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from receitas.views import home, contato, sobre
+from receitas.views import home
 
 urlpatterns = [
     path('', home),
-    path('contato/', contato),
-    path('sobre/', sobre),
 ]
