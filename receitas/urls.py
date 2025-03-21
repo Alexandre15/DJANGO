803b8d93c553314d5148d9d821dda269492887b3
Alexandre Fagundes <alefagundes155@gmail.com>
2025-03-20 23:48:39 -0300

    updateDjangoUdemy

--- receitas/urls.py
+++ receitas/urls.py
@@ -3,5 +3,5 @@ from receitas import views
 
 urlpatterns = [
     path('', views.home),
-    path('receitas/<int:id>/', views.receita)
+    path('receitas/<int:id>/', views.receita),
 ]
--- receitas/views.py
+++ receitas/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from utils.receitas.factory import make_recipe
 
 
 # Create your views here.
@@ -7,8 +8,8 @@ from django.http import HttpResponse
 # HTTP REQUEST
 def home(request):
     # return HttpResponse("<h1>HOME views</h1>")
-    return render(request, 'receitas/pages/home.html', context={'name': 'Alexandre Fagundes'}) # Variável "name" para o template HOME
+    return render(request, 'receitas/pages/home.html', context={'recipes': [make_recipe() for _ in range(10)]}) # Variável "name" para o template HOME
 
 def receita(request, id):
     # return HttpResponse("<h1>HOME views</h1>")
-    return render(request, 'receitas/pages/receita.html', context={'name': 'Alexandre Fagundes'}) # Variável "name" para o template HOME
+    return render(request, 'receitas/pages/receita.html', context={'recipe': make_recipe()}) # Variável "name" para o template HOME
